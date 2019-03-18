caf7e55fa86c3b7eed5fcd4c2aeb294c97f488cc
felixsoum <felix@felixsoum.com>
2019-03-18 08:06:46 -0400

    Add Cardstone

---
+++ Cardstone/Assets/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardData : IComparable
+{
+    public Color color = Color.red;
+    public int value = 10;
+
+    public CardData(Color color, int value)
+    {
+        this.color = color;
+        this.value = value;
+    }
+
+    public int CompareTo(object obj)
+    {
+        CardData otherCard = (CardData)obj;
+
+        if (color != otherCard.color)
+        {
+            return color == Color.black ? 1 : -1;
+        }
+        else
+        {
+            return value.CompareTo(otherCard.value);
+        }
+    }
+}
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] Text cardText;
+
+    public void SetData(CardData cardData)
+    {
+        cardText.text = cardData.value.ToString();
+        cardText.color = cardData.color;
+    }
+}
---
+++ Cardstone/Assets/Dealer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dealer : MonoBehaviour
+{
+    [SerializeField] Card[] cards = new Card[8];
+    List<CardData> cardData = new List<CardData>()
+    {
+        new CardData(Color.red, 2),
+        new CardData(Color.black, 2),
+        new CardData(Color.red, 5),
+        new CardData(Color.black, 5),
+        new CardData(Color.black, 3),
+        new CardData(Color.red, 4),
+        new CardData(Color.black, 4),
+        new CardData(Color.red, 3),
+    };
+
+    private void Awake()
+    {
+        SetData();
+    }
+
+    public void Sort()
+    {
+        cardData.Sort();
+        SetData();
+    }
+
+    private void SetData()
+    {
+        for (int i = 0; i < cards.Length; i++)
+        {
+            cards[i].SetData(cardData[i]);
+        }
+    }
+
+    public void Shuffle()
+    {
+        for (int i = cards.Length - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            var temp = cardData[i];
+            cardData[i] = cardData[j];
+            cardData[j] = temp;
+        }
+        SetData();
+    }
+}
